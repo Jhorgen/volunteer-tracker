9f977519f125e378a58fb444e6c369d5d0de3d9f
Josh <Joshua.horgen@gmail.com>
2019-08-11 15:11:04 -0700

    Add more CRUD to project.rb & volunteer.rb files

--- lib/project.rb
+++ lib/project.rb
@@ -8,8 +8,8 @@ class Project
     @title = attributes[:title]
   end
 
-  def ==(equals)
-    @title == equals.title
+  def ==(project)
+    @title == project.title
   end
 
   def save
@@ -21,15 +21,33 @@ class Project
     DB.exec("DELETE FROM projects WHERE id = #{@id};")
   end
 
+  def self.clear
+    DB.exec("DELETE FROM projects *;")
+  end
+
   def update(title)
     @title = title
     db.exec("UPDATE projects SET name = '#{@title}' WHERE id = #{@id};")
   end
 
   def self.all
-    db_records = DB.exec("SELECT * FROM projects")
-    db_records.map { |db_record| Project.new_from_db(db_record) }
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
   end
+
+  def self.find(id)
+    project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
+    title = project.fetch("title")
+    id = project.fetch("id").to_i
+    Project.new({:title => title, :id => id})
+  end
+
 
 
 
--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -4,12 +4,12 @@ class Volunteer
   attr_reader :name, :project_id, :id
 
   def initialize(attributes)
-    @id = attributes.fetch(:project_id)
-    @name = attributes.fetch(:name)
+    @id = attributes[:project_id]
+    @name = attributes[:name]
   end
 
-  def ==(equal)
-    @name == equal.name
+  def ==(volunteer)
+    @name == volunteer.name
   end
 
   def save
@@ -21,9 +21,31 @@ class Volunteer
     DB.exec("DELETE FROM volunteers WHERE id = #{@id};")
   end
 
+  def self.clear
+    DB.exec("DELETE FROM  *;")
+  end
+
   def update(name)
     @name = name
     db.exec("UPDATE volunteers SET name = '#{@name}' WHERE id = #{@id};")
+  end
+
+  def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Project.new({:name => name, :id => id}))
+    end
+    volunteers
+  end
+
+  def self.find(id)
+    volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
+    name = volunteer.fetch("name")
+    id = volunteer.fetch("id").to_i
+    Volunteer.new({:name => name, :id => id})
   end
 
 
